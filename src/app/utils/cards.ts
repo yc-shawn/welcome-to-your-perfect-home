46e1ca1b8fae802996b95ff5fb13144e3ae9e005
Shawn Chen <ethanloatrchen@gmail.com>
2019-10-17 01:02:06 -0700

    Box shadow styles

---
+++ src/app/utils/cards.ts
@@ -0,0 +1,12 @@
+export const PARKES = [1, 2, 4, 5, 5, 6, 7, 7, 8, 8, 9, 10, 11, 12, 14, 15];
+
+export const FENCES = [1, 3, 5, 5, 6, 6, 7, 8, 8, 9, 10, 10, 11, 11, 14, 15];
+
+export const BISES = [3, 4, 6, 7, 8, 9, 10, 12, 13];
+
+export const AGENCIES = [3, 4, 6, 7, 9, 10, 12, 13];
+
+export const POOLS = [4, 6, 7, 8, 9, 12];
+
+export const ESTATES = [1, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10, 11, 11, 12, 14, 15];
+
