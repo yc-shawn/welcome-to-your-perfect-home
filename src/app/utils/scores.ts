956cb882ed6f4b835e37c72fa9ac58a229ab85be
Shawn Chen <ethanloatrchen@gmail.com>
2019-11-04 18:12:46 -0800

    Add scores

---
+++ src/app/utils/scores.ts
@@ -0,0 +1,24 @@
+export const PARK_SCORE = [
+  [0, 2, 4, 10],
+  [0, 2, 4, 6, 14],
+  [0, 2, 4, 6, 8, 18],
+];
+
+export const POOL_SCORE = [0, 3, 6, 9, 13, 17, 21, 26, 31, 36];
+
+export const AGENCY_SCORE = [7, 4, 1];
+
+export const ESTATE_SCORE = [
+  [1, 3],
+  [2, 3, 4],
+  [3, 4, 5, 6],
+  [4, 5, 6, 7, 8],
+  [5, 6, 7, 8, 10],
+  [6, 7, 8, 10, 12],
+];
+
+export const BIS_SCORE = [0, 1, 3, 6, 9, 12, 16, 20, 24, 28];
+
+export const ROUNDABOUT_SCORE = [0, 3, 8];
+
+export const REFUSAL_SCORE = [0, 0, 3, 5];
